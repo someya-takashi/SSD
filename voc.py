bb650814fbb39ee4f6e41958bf3835b680e87d27
someya-takashi <someya.takashi.inter@gmail.com>
2022-02-06 18:49:14 +0900

    "update 20220226"

--- voc.py
+++ voc.py
@@ -1,5 +1,7 @@
 import os.path as osp
 
+from cv2 import resize
+
 # 画像データとアノテーションデータのパスを取得する関数
 def make_filepath_list(rootpath):
     # 画像ファイルとアノテーションファイルへのパスのテンプレートを作成
@@ -76,4 +78,65 @@ class GetBBoxAndLabel(object):
         return np.array(annotation)
 
 
+# イメージとアノテーションの前処理を行うDataTransformクラス
 
+from augmentations import Compose, ConvertFromInts, ToAbsoluteCoords, PhotometricDistort, Expand, RandomSampleCrop, RandomMirror, ToPercentCoords, Resize, SubtractMeans
+
+class DataTransform(object):
+    def __init__(self, input_size, color_mean):
+        self.transform = {
+            "train" : Compose([
+                ConvertFromInts(),
+                ToAbsoluteCoords(),
+                PhotometricDistort(),
+                Expand(color_mean),
+                RandomSampleCrop(),
+                RandomMirror(),
+                ToPercentCoords(),
+                Resize(input_size),
+                SubtractMeans(color_mean)
+            ]),
+            "val" : Compose([
+                ConvertFromInts(),
+                Resize(input_size),
+                SubtractMeans(color_mean)
+            ])
+        }
+    
+    def __call__(self, img, phase, boxes, labels):
+        return self.transform[phase](img, boxes, labels)
+
+import torch
+import torch.utils.data as data
+import cv2
+
+class PreProcessedVOC2012(data.Dataset):
+    def __init__(self, img_list, anno_list, phase, transform, get_bbox_label):
+        self.img_list = img_list
+        self.anno_list = anno_list
+        self.phase = phase
+        self.transform = transform
+        self.get_bbox_label = get_bbox_label
+
+    def __len__(self):
+        return len(self.img_list)
+    
+    def __getitem__(self, index):
+        im, bl, _, _ = self.pull_item(index)
+        return im, bl
+    
+    def pull_item(self, index):
+        img_path = self.img_list[index]
+        img = cv2.imread(img_path)
+        height, width, _ = img.shape
+
+        anno_file_path = self.anno_list[index]
+        bbox_label = self.get_bbox_label(anno_file_path, width, height)
+
+        img, boxes, labels = self.transform(img, self.phase, bbox_label[:, :4], bbox_label[:, 4])
+
+        img = torch.from_numpy(img[:, :, (2, 1, 0)]).permute(2, 0, 1)
+        boxlbl = np.stack((boxes, np.expand_dims(labels, axis=1)))
+
+        return img, boxlbl, height, width
+        
